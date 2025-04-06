b4d1587cc678cdec5dcc9933fc5493276b23cb7b
Hanhee Lee <hanhee@hotmail.ca>
2025-04-05 23:25:10 -0400

    Fix asset loading in production build

--- engsci-platform/src/pages/y1f/ESC180.tsx
+++ engsci-platform/src/pages/y1f/ESC180.tsx
@@ -173,7 +173,6 @@ const FlowChart = () => {
 const ESC180 = () => {
   return (
     <div className="w-full h-[calc(100vh-theme(spacing.16)-theme(spacing.16))] flex flex-col">
-
       <div className="relative flex-grow w-full">
         <ReactFlowProvider>
           <FlowChart />
