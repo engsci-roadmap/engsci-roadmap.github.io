9fc3b1f298e02930b747e0d27a664a3684edb306
Hanhee Lee <hanhee@hotmail.ca>
2025-04-05 23:17:42 -0400

    update

--- engsci-platform/src/pages/Home.tsx
+++ engsci-platform/src/pages/Home.tsx
@@ -3,7 +3,7 @@ const Home = () => {
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <div className="text-center">
         <h1 className="text-4xl font-bold text-gray-900 mb-4">
-          EngSci CheatCode
+          EngSci CheatCode {/* TODO: Add version number */}
         </h1>
         <p className="text-lg text-gray-600 mb-8">
           Your one-stop resource for Engineering Science materials
